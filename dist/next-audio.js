6da5a88e7092c3ae50784c3a78db6696fe1f178b
afeiship <1290657123@qq.com>
2019-05-07 23:16:34 +0800

    add: licence

--- dist/next-audio.js
+++ dist/next-audio.js
@@ -1,16 +1,34 @@
+/*!
+ * name: next-audio
+ * @link: https://github.com/afeiship/next-audio
+ * @version: 1.0.0
+ * @license: MIT
+ */
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var PROP_HOOKS = {
-    rate: 'playbackRate'
+    rate: 'playbackRate',
+    current: 'currentTime'
   };
 
   var NxAudio = nx.declare('nx.Audio', {
+    properties: {
+      times: {
+        get: function() {
+          var el = this.element;
+          return {
+            current: el.currentTime,
+            total: el.duration
+          };
+        }
+      }
+    },
     methods: {
       init: function(inOptions) {
         this.element = inOptions.element;
       },
-      // loop/volume/rate
+      // loop/volume/rate/current
       prop: function(inKey, inValue) {
         var key = PROP_HOOKS[inKey] || inKey;
         if (typeof inValue === 'undefined') {
